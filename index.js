2cdb3b9d430182bad520ce9356fe89dbe522d65f
Zac Willmington <zac.willmington@gmail.com>
2018-09-12 00:01:21 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,62 @@
 
+class Driver {
+    constructor (name, startDate) {
+        this.name = name;
+        this.startDate = new Date(startDate);
+    }
+
+    yearsExperienceFromBeginningOf (endDate) {
+         return  endDate - this.startDate.getFullYear() - 1;
+    }
+}
+
+class Route {
+    constructor (beginningLocation, endingLocation) {
+        this.beginningLocation = beginningLocation;
+        this.endingLocation = endingLocation;
+    };
+
+    distanceTaveledEastWest () {
+        let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+
+    };
+
+    distanceTaveledNorthSouth () {
+        return this.beginningLocation.vertical - this.endingLocation.vertical;
+    };
+
+
+    blocksTravelled (){
+        let eastWest =  ['1st Avenue', '2nd Avenue', '3rd Avenue', 'Lexington Avenue', 'Park', 'Madison Avenue', '5th Avenue'];
+
+         let blockStartNumber;
+         let blockEndNumber;
+         for (let i = 0; i < eastWest.length; i++){
+             if (eastWest[i] === this.beginningLocation.horizontal){
+                 blockStartNumber = i + 1;
+             }
+             if (eastWest[i] === this.endingLocation.horizontal){
+                 blockEndNumber = i + 1;
+             }
+         }
+
+         const distanceTaveledNorthSouth =  this.endingLocation.vertical - this.beginningLocation.vertical;
+         const distanceTaveledEastWest = blockEndNumber -  blockStartNumber;
+         const traveledTotal = distanceTaveledNorthSouth + distanceTaveledEastWest;
+
+         return traveledTotal;
+    };
+
+    estimatedTime (cond){
+        const at3BlocksPerMin = this.blocksTravelled() / 3;
+        const at2BlocksPerMin = this.blocksTravelled() / 2;
+        if (cond){
+            return at2BlocksPerMin;
+        }
+        return at3BlocksPerMin;
+    }
+
+
+
+}
